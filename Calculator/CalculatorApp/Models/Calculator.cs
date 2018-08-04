2a8ecefd7152274e7856463e6b13eae2701395dd
Emo <eiv.razgrad@gmail.com>
2018-08-04 17:23:16 +0300

    C# Web Calculator

---
+++ Calculator/CalculatorApp/Models/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CalculatorApp.Models
+{
+    public class Calculator
+    {
+        public decimal LeftOperand { get; set; }
+
+        public decimal RightOperand { get; set; }
+
+        public string Operator { get; set; }
+
+        public decimal Result { get; set; }
+
+        public void CalculateResult()
+        {
+            switch (this.Operator)
+            {
+                case "+":
+                    this.Result = this.LeftOperand + this.RightOperand;
+                    break;
+                case "-":
+                    this.Result = this.LeftOperand - this.RightOperand;
+                    break;
+                case "*":
+                    this.Result = this.LeftOperand * this.RightOperand;
+                    break;
+                case "/":
+                    this.Result = this.LeftOperand / this.RightOperand;
+                    break;
+                case "%":
+                    this.Result = this.LeftOperand % this.RightOperand;
+                    break;
+                case "^":
+                    int a = Convert.ToInt32(this.RightOperand);
+                    int b = Convert.ToInt32(this.LeftOperand);
+                    this.Result = (int)Math.Pow(b, a);
+                    break;
+            }
+            
+        }
+
+    }
+}
